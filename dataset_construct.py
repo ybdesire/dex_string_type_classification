32e9982c5db2f9052354c2acf36d1d68aba06c57
ybdesire <ybdesire@gmail.com>
2017-08-28 08:24:23 +0800

    Init the feature extraction code.

---
+++ dataset_construct.py
@@ -0,0 +1,21 @@
+import os
+import joblib as jl
+
+
+def extract_feature_to_dataset():
+    orig_data_path = 'str_txt'
+    files = os.listdir(orig_data_path)
+    for f in files:
+        with open( os.path.join(orig_data_path, f), 'r' ) as fr:
+            feadic = eval(fr.read())
+            for k in feadic:
+                v = k.encode('raw_unicode_escape').decode('utf8')
+
+
+
+if __name__=='__main__':
+    print('asdf')
+
+
+
+    
