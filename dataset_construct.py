c34f3f1c695db7c48ef1884af16e46181e019e5b
ybdesire <ybdesire@gmail.com>
2017-09-06 08:21:49 +0800

    Get labels distribuitons.

--- dataset_construct.py
+++ dataset_construct.py
@@ -22,14 +22,29 @@ def get_string_len_distribution():
     # {0:16, 50:3292, 228:7, 4044:1}
     # then set max_len for zero-padding
 
+def get_labels_distribution():
+    orig_data_path = 'str_txt'
+    files = os.listdir(orig_data_path)
+    
+    # get the string len distribution
+    labels_distribution = set()
+    for f in files:
+        with open( os.path.join(orig_data_path, f), 'r' ) as fr:
+            feadic = eval(fr.read())
+            for k in feadic:
+                labels_distribution.add( tuple(feadic[k]) )
+    print(labels_distribution)
+    # {('type',), ('type', 'field_name'), ('method_name', 'type'), ('method_name', 'type', 'field_name'),
+    # (), ('method_name',), ('method_name', 'field_name'), ('field_name',)}
 
+    
 def extract_feature_to_dataset():
     pass
     
     
 
 if __name__=='__main__':
-    extract_feature_to_dataset()
+    get_labels_distribution()
 
 
 
